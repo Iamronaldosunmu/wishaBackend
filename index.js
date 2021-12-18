163ff7ef65fd1b1921229c240f084478c6f4bea8
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-18 12:05:11 +0000

    Finshed the post endpoints

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const express = require('express');
 const app = express();
+const emails = require('./routes/emails');
+
+app.use(express.json());
+app.use('/api/emails', emails);
+
 const port = process.env.PORT || 3000;
 
-app.get('/', function (req, res) {
-    res.send('It is finally working');
-})
-app.get('/:id/age?sortBy=named', function (req, res) {
-    res.send(req.params.query);
-})
+
 
 app.listen(port, () => {console.log(`Now listening on port ${port}`)});
