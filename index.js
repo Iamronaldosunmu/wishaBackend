913b6c79faa14981bd7a5585ad07b0ef45317ad8
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-18 12:23:19 +0000

    set up config folder

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const app = express();
 const emails = require('./routes/emails');
+const config = require('config');
 
 app.use(express.json());
 app.use('/api/emails', emails);
 
-const port = process.env.PORT || 3000;
+const port = config.get('port') || 3000;
 
 
 
