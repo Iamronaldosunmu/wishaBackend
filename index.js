fcbe14dfdec9fe58f8d4073ce791c05dfe1aaf1f
Ronald Dosunmu <ronaldosunmu@gmail.com>
2021-12-24 17:50:53 +0000

    created prod.js

--- index.js
+++ index.js
@@ -6,10 +6,13 @@ require('express-async-errors');
 process.on('unhandledRejection', (ex) => {
     throw ex;
   });
+process.on('uncaughtException', (ex) => {
+    throw ex;
+  });
 require('./startup/cors')(app);
 require('./startup/routes')(app);
 require('./startup/db')();
-
+require('./startup/prod')(app);
 
 const port = config.get('port') || 3000;
 
